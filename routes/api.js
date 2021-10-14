cb6f70d5ae14191b3c7399ec1a54cbe3f0b4270b
Tucker Barrett <tuckerbarrett@gmail.com>
2021-10-14 14:12:25 -0400

    Fixes put and post routes in routes/api.js

--- routes/api.js
+++ routes/api.js
@@ -2,15 +2,43 @@ const router = require("express").Router();
 const { Workout } = require("../models");
 
 
-router.get("/api/workouts", (req, res) => {
+// Get all workouts
+router.get("/workouts", (req, res) => {
   Workout.find({})
-    .sort({ date: -1 })
+    .sort({ day: 1 })
     .then(dbWorkout => {
       res.json(dbWorkout);
     })
     .catch(err => {
       res.status(400).json(err);
     });
+});
+
+
+router.put('/workouts/:id', (req,res) => {
+  Workout.findOneAndUpdate(
+    { _id: req.params.id },
+    { $push: { exercises: req.body } },
+    function (error, success) {
+        if (error) {
+            res.status(400).json(error);
+        } else {
+            res.json(success);
+        }
+    }
+  );
+})
+
+
+// Create a new workout
+router.post('/workouts', async (req, res) => {
+  const newWorkout = new Workout(req.body);
+  try {
+    await newWorkout.save();
+    res.send(newWorkout);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 module.exports = router;
